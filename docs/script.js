1c44770d7c436f1fd9daafa477b5987f077df6d1
Nicola Renzi <nico.renzi22@gmail.com>
2026-03-01 12:48:12 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -685,7 +685,7 @@ class AboutSheet {
 
       // tiny “peek” so user notices it’s draggable (optional)
       if (!this.isLatched && !this.dragging) {
-        this.apply(0.06);
+        this.apply(0.22);
       }
     } else {
       this.sheet.classList.remove('is-armed');
